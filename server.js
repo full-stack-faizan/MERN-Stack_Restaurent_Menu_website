59567186cae8baf6d7fbf0996d95921282021ec6
Syed Faizan <getinforweblanguages@gmail.com>
2023-08-25 10:41:40 +0500

    nere

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(express.static(path.join(__dirname + "/frontend/build")));
 
 // app.get("*",(req,res)=>{
 // res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+
 // })
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
