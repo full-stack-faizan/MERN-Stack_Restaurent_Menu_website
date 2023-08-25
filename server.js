864f056fefba3b6e3cdffef6d970cd8d15624fab
Syed Faizan <getinforweblanguages@gmail.com>
2023-08-25 10:23:49 +0500

    new

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path= require('path')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 4000
+const router = require('./src/Routes/route')
+const cors = require('cors')
+app.use(cors())
+app.use(router)
+
+require('./src/Database/db')
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*",(req,res)=>{
+res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`express is successful http://localhost:4000/data/show`)
+})
+
+
+
+
+
+console.log('this is express')
