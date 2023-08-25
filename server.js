5f0fb6cd00dd6ff1f1ca70f194b742ad10ff1409
Syed Faizan <getinforweblanguages@gmail.com>
2023-08-25 10:39:32 +0500

    sdf

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ app.use(router)
 
 require('./src/Database/db')
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname + "/frontend/build")));
 
-app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
-})
+// app.get("*",(req,res)=>{
+// res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+// })
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
 
-
-
+// console.log(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 
 app.listen(port, ()=>{
     console.log(`express is successful http://localhost:4000/data/show`)
